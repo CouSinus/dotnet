71dd1bc4f918707444929502083f8d7fed7d7171
rissmaxime <maxime.riss@cpe.fr>
2022-09-23 08:50:26 +0200

    controller getAll et getById

---
+++ WepAPITP1/Controllers/DevisesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WepAPITP1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WepAPITP1.Controllers
+{
+    [Route("api/Devise")]
+    [ApiController]
+    public class DevisesController : ControllerBase
+    {
+        private List<Devise> listDevises;
+
+        public List<Devise> ListDevises { get => listDevises; set => listDevises = value; }
+
+        public DevisesController()
+        {
+            listDevises = new List<Devise>();
+            ListDevises.Add(new Devise(1, "Dollar", 1.08));
+            ListDevises.Add(new Devise(2, "Franc Suisse", 1.07));
+            ListDevises.Add(new Devise(3, "Yen", 120));
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Devise> GetAll()
+        {
+            return ListDevises;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}", Name = "GetDevise")]
+        public Devise GetById(int id)
+        {
+            return ListDevises.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WepAPITP1/Models/Devise.cs
+++ WepAPITP1/Models/Devise.cs
@@ -2,5 +2,40 @@
 {
     public class Devise
     {
-    }
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string? deviseName;
+
+		public string? DeviseName
+		{
+			get { return deviseName; }
+			set { deviseName = value; }
+		}
+
+		private double taux;
+
+		public double Taux
+		{
+			get { return taux; }
+			set { taux = value; }
+		}
+
+		public Devise(int id, string deviseName, double taux)
+		{
+			Id = id;
+			DeviseName = deviseName;
+            Taux = taux;
+        }
+
+		public Devise()	{}
+
+
+
+	}
 }
