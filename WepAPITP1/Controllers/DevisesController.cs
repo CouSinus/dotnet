6c59f75e3f19d74337b7ebb986f6a9c87dfa3390
rissmaxime <maxime.riss@cpe.fr>
2022-09-23 10:07:32 +0200

    Ajout des commentaires XML

--- WepAPITP1/Controllers/DevisesController.cs
+++ WepAPITP1/Controllers/DevisesController.cs
@@ -22,6 +22,10 @@ namespace WepAPITP1.Controllers
             ListDevises.Add(new Devise(3, "Yen", 120));
         }
 
+        /// <summary>
+        /// Get all currencies
+        /// </summary>
+        /// <returns>List of currencies</returns>
         // GET: api/<ValuesController>
         [HttpGet]
         public IEnumerable<Devise> GetAll()
@@ -29,16 +33,34 @@ namespace WepAPITP1.Controllers
             return ListDevises;
         }
 
+        /// <summary>
+        /// Get a single currency
+        /// </summary>
+        /// <param name="id">Currency id</param>
+        /// <returns>HttpResponse</returns>
+        /// <response code ="200">Found a currency</response>
+        /// <response code ="404">No currency found</response>
         // GET api/<ValuesController>/5
         [HttpGet("{id}", Name = "GetDevise")]
+        [ProducesResponseType(typeof(Devise), 200)]
+        [ProducesResponseType(404)]
         public IActionResult GetById(int id)
         {
             Devise devise = ListDevises.FirstOrDefault(x => x.Id == id);
             return devise == null ? NotFound() : Ok(devise);
         }
 
+        /// <summary>
+        /// Save a new currency
+        /// </summary>
+        /// <param name="devise">Currency</param>
+        /// <returns>HttpResponse</returns>
+        /// <response code ="200">Currency saved</response>
+        /// <response code ="400">Invalid format of currency</response>
         // POST api/<ValuesController>
         [HttpPost]
+        [ProducesResponseType(typeof(Devise), 200)]
+        [ProducesResponseType(400)]
         public IActionResult Post([FromBody] Devise devise)
         {
             if(!ModelState.IsValid)
@@ -49,8 +71,20 @@ namespace WepAPITP1.Controllers
             return CreatedAtRoute("GetDevise", new { id = devise.Id }, devise);
         }
 
+        /// <summary>
+        /// Edit a currency
+        /// </summary>
+        /// <param name="id">Currency id</param>
+        /// <param name="devise">Currency</param>
+        /// <returns>HttpResponse</returns>
+        /// <response code ="200">Currency saved</response>
+        /// <response code ="400">Invalid format of currency</response>
+        /// <response code ="404">Currency not found</response>
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
+        [ProducesResponseType(typeof(Devise), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
         public IActionResult Put(int id, [FromBody] Devise devise)
         {
             if (!ModelState.IsValid) { return BadRequest(ModelState); }
@@ -63,8 +97,17 @@ namespace WepAPITP1.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Delete a currency
+        /// </summary>
+        /// <param name="id">Currency id</param>
+        /// <returns>HttpResponse</returns>
+        /// <response code ="200">Currency saved</response>
+        /// <response code ="404">Currency not found</response>
         // DELETE api/<ValuesController>/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(Devise), 200)]
+        [ProducesResponseType(404)]
         public IActionResult Delete(int id)
         {
             Devise devise = (from d in ListDevises where d.Id == id select d).FirstOrDefault();
